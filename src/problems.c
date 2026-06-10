8955fee976a1c6698ed84718c607d96ad6424266
Nils Friess <nils.friess@gmail.com>
2026-06-10 15:05:18 +0200

    Use Neumann BC's in finite difference shifted Laplace example

--- src/problems.c
+++ src/problems.c
@@ -24,34 +24,47 @@ PetscErrorCode MatAssembleShiftedLaplaceFD(DM dm, PetscReal kappa, Mat mat)
   hinv2 = 1. / ((mx - 1) * (mx - 1));
   for (PetscInt j = ys; j < ys + ym; j++) {
     for (PetscInt i = xs; i < xs + xm; i++) {
+      PetscReal diag = kappa * kappa;
+      PetscInt  n    = 0;
+
       row.j = j;
       row.i = i;
-      if (i == 0 || j == 0 || i == mx - 1 || j == my - 1) {
-        vals[0] = 1;
-        PetscCall(MatSetValuesStencil(mat, 1, &row, 1, &row, vals, INSERT_VALUES));
-      } else {
-        cols[0].j = j - 1;
-        cols[0].i = i;
-        vals[0]   = -hinv2;
 
-        cols[1].j = j;
-        cols[1].i = i - 1;
-        vals[1]   = -hinv2;
-
-        cols[2].j = j;
-        cols[2].i = i;
-        vals[2]   = 4 * hinv2 + kappa * kappa;
-
-        cols[3].j = j + 1;
-        cols[3].i = i;
-        vals[3]   = -hinv2;
-
-        cols[4].j = j;
-        cols[4].i = i + 1;
-        vals[4]   = -hinv2;
-
-        PetscCall(MatSetValuesStencil(mat, 1, &row, 5, cols, vals, INSERT_VALUES));
+      if (j > 0) {
+        cols[n].j = j - 1;
+        cols[n].i = i;
+        vals[n]   = -hinv2;
+        diag += hinv2;
+        n++;
       }
+      if (i > 0) {
+        cols[n].j = j;
+        cols[n].i = i - 1;
+        vals[n]   = -hinv2;
+        diag += hinv2;
+        n++;
+      }
+      if (j < my - 1) {
+        cols[n].j = j + 1;
+        cols[n].i = i;
+        vals[n]   = -hinv2;
+        diag += hinv2;
+        n++;
+      }
+      if (i < mx - 1) {
+        cols[n].j = j;
+        cols[n].i = i + 1;
+        vals[n]   = -hinv2;
+        diag += hinv2;
+        n++;
+      }
+
+      cols[n].j = j;
+      cols[n].i = i;
+      vals[n]   = diag;
+      n++;
+
+      PetscCall(MatSetValuesStencil(mat, 1, &row, n, cols, vals, INSERT_VALUES));
     }
   }
 
