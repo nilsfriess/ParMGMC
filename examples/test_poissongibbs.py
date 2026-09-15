23e14f6767aa00a1890c24d6f73d8f820b601f50
Eike Hermann Mueller <e.mueller@bath.ac.uk>
2026-09-15 11:25:56 +0100

    Updated tests to be consistent with changes to library

--- examples/test_poissongibbs.py
+++ examples/test_poissongibbs.py
@@ -19,7 +19,9 @@ class MarginalisedDistribution:
 
     for SPD :math:`2\\times 2` pecision matrix :math:`A` and right hand
     side vector :math:`f`. The rate is given by
-    :math:`\\Lambda(y) = e^{b y +\\nu}`. Marginalising over :math:`y_2`
+    :math:`\\Lambda(y) = e^{b y -\\nu}` with :math:`\\nu = -\\beta - \\log(t)` where
+    :math:`\\beta` is a scalar offset and :math:`t` is the measurement interval.
+    Marginalising over :math:`y_2`
     gives
 
     .. math::
@@ -34,7 +36,9 @@ class MarginalisedDistribution:
     LOWER_BOUND = -100
     UPPER_BOUND = +100
 
-    def __init__(self, Q_prec, f_rhs, b_measurement, nu, event_count):
+    def __init__(
+        self, Q_prec, f_rhs, b_measurement, beta, event_count, measurement_interval
+    ):
         """Initialise new instance
 
         Parameters
@@ -45,15 +49,17 @@ class MarginalisedDistribution:
             right hand side vector :math:`f`
         b_measurement :
             strength of measurement :math:`b`
-        nu :
-            offset :math:`nu`
+        beta :
+            offset :math:`beta`
         event_count :
             measured number of events :math:`n`
+        measurement_interval :
+            measurement interval
         """
         self._Q_prec = Q_prec
         self._f_rhs = f_rhs
         self._b_measurement = b_measurement
-        self._nu = nu
+        self._nu = -beta - np.log(measurement_interval)
         self._event_count = event_count
         # normalise marginal distribution
         self._Znorm = (
@@ -80,7 +86,7 @@ class MarginalisedDistribution:
         =======
         Value of unnormalised PDF :math:`Z p_{mar}(y)`
         """
-        Lambda = np.exp(self._b_measurement * y + self._nu)
+        Lambda = np.exp(self._b_measurement * y - self._nu)
         p_poisson = Lambda**self._event_count * np.exp(-Lambda)
         p_marginal = np.exp(
             -1
@@ -175,10 +181,10 @@ class Sampler:
 
     for SPD :math:`2\\times 2` pecision matrix :math:`A` and right hand
     side vector :math:`f`. The rate is given by
-    :math:`\\Lambda(y) = e^{b y +\\nu}`.
+    :math:`\\Lambda(y) = e^{b y - \\nu}`.
     """
 
-    def __init__(self, Q, f, b, nu, event_count):
+    def __init__(self, Q, f, b, beta, event_count, measurement_interval):
         """Initialise new instance
 
         Parameters
@@ -189,17 +195,19 @@ class Sampler:
             right hand side vector :math:`f`
         b_measurement :
             strength of measurement :math:`b`
-        nu :
-            offset :math:`nu`
+        beta :
+            offset :math:`\\beta`
         event_count :
             measured number of events :math:`n`
+        measurement_interval :
+            measurement interval :math:`t`
         """
         # PETSc objects
         Q_petsc = PETSc.Mat().createAIJWithArrays(
             (2, 2), ((0, 2, 4), (0, 1, 0, 1), [Q[0, 0], Q[0, 1], Q[1, 0], Q[1, 1]])
         )
         B_petsc = PETSc.Mat().createAIJWithArrays((2, 1), ((0, 1, 2), (0, 0), [b, 0]))
-        nu_petsc = PETSc.Vec().createWithArray([nu])
+        measurement_interval_petsc = PETSc.Vec().createWithArray([measurement_interval])
         self._b_rhs_petsc = PETSc.Vec().createWithArray(f)
         event_count_petsc = PETSc.Vec().createWithArray([event_count])
         snes = PETSc.SNES().create()
@@ -211,7 +219,14 @@ class Sampler:
         }
         for key, value in solver_parameters.items():
             opts[key] = value
-        pymgmc.SNESPoissonSetAppCtx(snes, event_count_petsc, Q_petsc, B_petsc, nu_petsc)
+        pymgmc.SNESPoissonSetAppCtx(
+            snes,
+            event_count_petsc,
+            measurement_interval_petsc,
+            beta,
+            Q_petsc,
+            B_petsc,
+        )
         snes.setFromOptions()
         self._snes = snes
         self._y = PETSc.Vec().createWithArray([0, 0])
@@ -227,8 +242,9 @@ def visualise(
     Q_prec,
     f_rhs,
     b_measurement,
-    nu,
+    beta,
     event_count,
+    measurement_interval,
     n_samples=10000,
     n_bins=128,
     y_min=-3.0,
@@ -245,10 +261,12 @@ def visualise(
         right hand side vector :math:`f`
     b_measurement :
         strength of measurement :math:`b`
-    nu :
-        offset :math:`nu`
+    beta :
+        offset :math:`\\beta`
     event_count :
         measured number of events :math:`n`
+    measurement_interval :
+        measurement interval :math:`t`
     n_samples :
         number of samples to use
     n_bins :
@@ -261,9 +279,11 @@ def visualise(
         name of file to save to
     """
     distribution = MarginalisedDistribution(
-        Q_prec, f_rhs, b_measurement, nu, event_count
+        Q_prec, f_rhs, b_measurement, beta, event_count, measurement_interval
     )
-    sampler = Sampler(Q_prec, f_rhs, b_measurement, nu, event_count)
+    sampler = Sampler(
+        Q_prec, f_rhs, b_measurement, beta, event_count, measurement_interval
+    )
     samples = np.asarray(list(itertools.islice(sampler, n_samples)))[:, 0]
     ks = distribution.kolmogorov_smirnov(samples)
     print(f"Kolmgorov-Smirnow statistics = {ks:6.2e}")
@@ -280,22 +300,26 @@ def visualise(
 
 # Configurations used for testing
 test_data = [
-    (np.array([[1.0, 0.0], [0.0, 1.0]]), np.array([0.0, 0.1]), 1.0, 0.0, 1),
-    (np.array([[1.0, -0.1], [-0.1, 2.0]]), np.array([0.2, 0.8]), 2.0, 0.5, 2),
-    (np.array([[1.0, -0.1], [-0.1, 2.0]]), np.array([0.2, 0.8]), 2.0, 0.5, 2),
-    (np.array([[1.0, 0.5], [0.5, 2.0]]), np.array([0.2, 0.8]), 2.0, 0.5, 2),
-    (np.array([[1.0, 0.5], [0.5, 2.0]]), np.array([0.2, 0.8]), 1.0, 0.0, 4),
+    (np.array([[1.0, 0.0], [0.0, 1.0]]), np.array([0.0, 0.1]), 1.0, 0.0, 1, 1),
+    (np.array([[1.0, -0.1], [-0.1, 2.0]]), np.array([0.2, 0.8]), 2.0, 0.5, 2, 1.5),
+    (np.array([[1.0, -0.1], [-0.1, 2.0]]), np.array([0.2, 0.8]), 2.0, 0.5, 2, 1.1),
+    (np.array([[1.0, 0.5], [0.5, 2.0]]), np.array([0.2, 0.8]), 2.0, 0.5, 2, 1),
+    (np.array([[1.0, 0.5], [0.5, 2.0]]), np.array([0.2, 0.8]), 1.0, 0.0, 4, 1.3),
 ]
 
 
-@pytest.mark.parametrize("Q_prec,f_rhs,b_measurement,nu,event_count", test_data)
-def test_sampler(Q_prec, f_rhs, b_measurement, nu, event_count):
+@pytest.mark.parametrize(
+    "Q_prec,f_rhs,b_measurement,beta,event_count,measurement_interval", test_data
+)
+def test_sampler(Q_prec, f_rhs, b_measurement, beta, event_count, measurement_interval):
     """Verify that samples satisfy the Kolmogorov Smirnov test"""
     n_samples = 20000
     distribution = MarginalisedDistribution(
-        Q_prec, f_rhs, b_measurement, nu, event_count
+        Q_prec, f_rhs, b_measurement, beta, event_count, measurement_interval
     )
-    sampler = Sampler(Q_prec, f_rhs, b_measurement, nu, event_count)
+    sampler = Sampler(
+        Q_prec, f_rhs, b_measurement, beta, event_count, measurement_interval
+    )
     samples = np.asarray(list(itertools.islice(sampler, n_samples)))[:, 0]
     tolerance = 1.0e-2
     assert distribution.kolmogorov_smirnov(samples) < tolerance
@@ -305,6 +329,9 @@ if __name__ == "__main__":
     A_precision = np.array([[1.0, -0.1], [-0.1, 2.0]])
     f_rhs = np.array([0.2, 0.8])
     b_measurement = 2.0
-    nu = 0.5
-    event_count = 1
-    visualise(A_precision, f_rhs, b_measurement, nu, event_count)
+    beta = 0.5
+    event_count = 2
+    measurement_interval = 1
+    visualise(
+        A_precision, f_rhs, b_measurement, beta, event_count, measurement_interval
+    )
