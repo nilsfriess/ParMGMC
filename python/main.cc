31bca33ae4b46e0c46baa018bb0984d24bb5b28f
Eike Mueller <eike.h.mueller@gmail.com>
2026-09-20 11:32:29 +0100

    Avoid re-setting user context

--- python/main.cc
+++ python/main.cc
@@ -53,8 +53,22 @@ PYBIND11_MODULE(pymgmc, m)
   });
   m.def("SetPoissonCtx", [](SNES snes, Vec event_counts, Vec measurement_intervals, PetscScalar beta, Mat Q_prec, Mat B_meas) {
     PoissonCtx *ctx;
+    Vec         marker; // marker which is attached to SNES to signal that user context has been set
 
     PetscFunctionBegin;
+    // Check whether context has already been attached to this SNES
+    // We do not want to set the user context twice since currently the snes setup routine uses information
+    // from this context, and this would become stale if the context is changed later
+    PetscCallVoid(PetscObjectQuery((PetscObject)snes, "ctx_set", (PetscObject *)&marker));
+    if (marker) {
+      PetscCallVoid(PetscErrorPrintf("User context for SNES already set\n"));
+      MPI_Abort(PETSC_COMM_WORLD, PETSC_ERR_COR);
+    } else {
+      PetscCallVoid(VecCreateSeq(PETSC_COMM_SELF, 1, &marker));
+      PetscCallVoid(PetscObjectCompose((PetscObject)snes, "ctx_set", (PetscObject)marker));
+      PetscCallVoid(VecDestroy(&marker));
+    }
+
     PetscCallVoid(PetscNew(&ctx));
     PetscCallVoid(PetscObjectReference((PetscObject)event_counts));
     PetscCallVoid(PetscObjectReference((PetscObject)Q_prec));
